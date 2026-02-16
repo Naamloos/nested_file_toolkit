576343e5cfc6064f20a87a33afd050aa507f992a
Ryan de Jonge <ryandejonge@outlook.com>
2026-02-16 10:53:49 +0100

    Add ThemeColor import for badge color

--- src/providers/file-children-count-provider.ts
+++ src/providers/file-children-count-provider.ts
@@ -1,4 +1,4 @@
-import { FileDecorationProvider, EventEmitter, Uri, workspace } from 'vscode';
+import { FileDecorationProvider, EventEmitter, Uri, ThemeColor, workspace } from 'vscode';
 import { FileDecoration } from 'vscode';
 import { detectChildFiles } from '../util/detect-child-files';
 import { detectParentFiles } from '../util/detect-parent-files';
@@ -54,6 +54,7 @@ export class FileChildCountProvider implements FileDecorationProvider {
     return {
       badge: String(count) + '\u2193', // down arrow
       tooltip: `${count} nested file${count !== 1 ? 's' : ''}`,
+      color: new ThemeColor('sideBar.foreground'),
     };
   }
 }
